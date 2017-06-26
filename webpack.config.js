3908edddc3da6060449f1d3c020d79498d95ab78
Alex Tumanov <uwontrememberthis@gmail.com>
2017-06-26 22:03:44 +0300

    Trying to set html watcher

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
   devtool: 'cheap-eval-source-map',
-  entry: APP_DIR + '/test.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
+    APP_DIR + '/test.js',
+  ],
   output: {
     path: BUILD_DIR,
     filename: "bundle.js",
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
   module: {
     loaders: [
       {
@@ -18,6 +29,14 @@ module.exports = {
         include: APP_DIR,
         loader: 'babel-loader',
       },
+      {
+        test: /\.html$/,
+        loader: "raw-loader",
+      },
     ],
+  },
+  devServer: {
+    contentBase: './build',
+    hot: true
   },
 };
